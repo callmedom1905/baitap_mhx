3632ef51d3ce727b1a91b071ecee4fe8f462f28d
callmedom1905 <minhtcps38149@gmail.com>
2024-11-25 09:09:55 +0700

    project router

---
+++ router/src/views/ContactView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <welcome-item />
+  </div>
+</template>
+
+<script>
+import WelcomeItem from "../components/WelcomeItem.vue";
+export default {
+  components: { WelcomeItem },
+
+}
+</script>
+
+<style>
+
+</style>
